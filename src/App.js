0795e9f2282f572e7e0e0cd147276caf6c367061
Jaden Goodwin <jaden.goodwin24@gmail.com>
2017-12-06 12:09:59 -0700

    routes finished

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/home.js';
 import Landing from './components/landing.js';
-
+import { Link } from 'react-router-dom';
 class App extends Component {
   render() {
     return (
@@ -12,6 +12,10 @@ class App extends Component {
           <Switch>
             <Route component={Landing} exact path='/' />
             <Route component={Home} path='/home' />
+            <Route render={() => <div><Link to='/hello'> <h2>Hello</h2> </Link>
+              <Link to='/'> <h2>Landing </h2></Link>
+              <Link to='/home'> <h2>Home </h2></Link>
+            <br/><h1>hello there people</h1></div>} path='/hello' />
           </Switch>
         </Router>
       </div>
--- src/components/landing.js
+++ src/components/landing.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './landing.css';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 class Landing extends Component {
     constructor() {
         super();
@@ -41,6 +42,11 @@ class Landing extends Component {
         })
         return (
             <div className='Landing'>
+                <div>
+                    <Link to='/hello'> <h2>Hello</h2> </Link>
+                    <Link to='/'> <h2>Landing </h2></Link>
+                    <Link to='/home'> <h2>Home </h2></Link>
+                </div>
                 animals{animals}
             </div>
         )
